34fd2fbc09a121913c39ed19c73eead94910f4af
Gonzagadavid <73129958+Gonzagadavid@users.noreply.github.com>
2021-01-23 13:46:35 -0300

    commit inicial

---
+++ funcoes.js
@@ -0,0 +1,110 @@
+const fs = require('fs')
+const { resolve } = require('path')
+const path = require('path')
+
+function lerDiretorio (caminho) {
+  return new Promise((resolve, reject) => {
+    try {
+      const arquivos = fs.readdirSync(caminho).map(arquivo => path.join(caminho, arquivo))
+      resolve(arquivos)
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
+
+function elementosTerminadoCom (padrão) {
+  return function (array) {
+    return array.filter(arquivo => arquivo.endsWith(padrão))
+  }
+}
+
+function lerArquivo (caminho) {
+  return new Promise((resolve, reject) => {
+    try {
+      const conteudo = fs.readFileSync(caminho, { encoding: 'utf-8' })
+      resolve(conteudo.toString())
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
+
+function lerArquivos (caminhos) {
+  return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
+}
+
+function removerElementosSeVazio (array) {
+  return array.filter(elemento => elemento.trim())
+}
+
+function removerElementosSeIcluir (padrão) {
+  return function (array) {
+    return array.filter(elemento => !elemento.includes(padrão))
+  }
+}
+
+function removerElementosSeIncluirNumeros (array) {
+  return array.filter(elemento => isNaN(Number(elemento)))
+}
+
+function removerSimbolos (simbolos) {
+  return function (array) {
+    return array.map(elemento => {
+      return simbolos.reduce((acc, crr) => {
+        return acc.split(crr).join('')
+      }, elemento)
+    })
+  }
+}
+
+function mesclarElementos (conteudo) {
+  return conteudo.join(' ')
+}
+
+function separarTextoPor (simbolo) {
+  return function (texto) {
+    return texto.split(simbolo)
+  }
+}
+
+function agruparElementos (palavras) {
+  return Object.values(palavras.reduce((acc, palavra) => {
+    const el = palavra.toLowerCase()
+    const qtde = acc[el] ? acc[el].qtde + 1 : 1
+    acc[el] = { elementos: el, qtde }
+    return acc
+  }, {}))
+}
+
+function ordenarPorAtributoNumerico (attr, ordem = 'asc') {
+  return function (array) {
+    const asc = (a, b) => b[attr] - a[attr]
+    const desc = (a, b) => b[attr] - a[attr]
+    return [...array].sort(ordem === 'asc' ? asc : desc)
+  }
+}
+
+function postarConteudoJson (caminho) {
+  return function (conteudo) {
+    fs.appendFile(caminho, JSON.stringify(conteudo), (err) => {
+      if (err) throw err
+      console.log('The "data to append" was appended to file!')
+    })
+  }
+}
+
+module.exports = {
+  lerDiretorio,
+  elementosTerminadoCom,
+  lerArquivos,
+  removerElementosSeVazio,
+  removerElementosSeIcluir,
+  removerElementosSeIncluirNumeros,
+  removerSimbolos,
+  mesclarElementos,
+  separarTextoPor,
+  agruparElementos,
+  ordenarPorAtributoNumerico,
+  postarConteudoJson
+}
---
+++ index.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const fn = require('./funcoes')
+
+const caminhoJson = path.join('./arquivos.json')
+const caminho = path.join(__dirname, './legendas')
+const simbolos = [
+  '.', '?', '-', '_', '♪', '"', '<i>', '</i>',
+  '<b>', '</b>', '\r', '[', ']', '(', ')', ','
+]
+
+fn.lerDiretorio(caminho)
+  .then(fn.elementosTerminadoCom('.srt'))
+  .then(fn.lerArquivos)
+  .then(fn.mesclarElementos)
+  .then(fn.separarTextoPor('\n'))
+  .then(fn.removerElementosSeVazio)
+  .then(fn.removerElementosSeIcluir('-->'))
+  .then(fn.removerElementosSeIncluirNumeros)
+  .then(fn.removerSimbolos(simbolos))
+  .then(fn.mesclarElementos)
+  .then(fn.separarTextoPor(' '))
+  .then(fn.removerElementosSeVazio)
+  .then(fn.removerElementosSeIncluirNumeros)
+  .then(fn.agruparElementos)
+  .then(fn.ordenarPorAtributoNumerico('qtde', 'desc'))
+  .then(fn.postarConteudoJson(caminhoJson))
